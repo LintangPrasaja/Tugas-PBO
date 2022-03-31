b173f5e8490450a81dd1fdee0e10ef47cc7b93ea
Lintang <101095976+LintangPrasaja@users.noreply.github.com>
2022-03-31 22:05:39 +0700

    Delete Tugas 3 directory

--- Tugas 3/03_Constructor.kt
+++
@@ -1,9 +0,0 @@
-class Mobil(var brand: String, var model: String, var year: Int)
-
-fun main() {
-    val c1 = Mobil("Kijang", "Mbee", 2013)
-
-    println(c1.brand)
-    println(c1.model)
-    println(c1.year)
-}
