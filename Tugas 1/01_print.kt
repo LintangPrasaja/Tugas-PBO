6316f3b199fc593eed4145b95e407a0d3f82dde9
Lintang <101095976+LintangPrasaja@users.noreply.github.com>
2022-03-31 22:04:52 +0700

    Delete Tugas 1 directory

--- Tugas 1/01_print.kt
+++
@@ -1,4 +0,0 @@
-fun main() {
-    println("Halo, Perkenalkan Nama Saya Alif Lintang Prasaja")
-    println("Dari Kelompok A11.4421")
-}
--- Tugas 1/02_comment.kt
+++
@@ -1,6 +0,0 @@
-fun main() {
-    // Ini adalah comment
-    // Disini saya memperkenalkan diri
-    println("Halo, Perkenalkan Nama Saya Alif Lintang Prasaja")
-    println("Dari Kelompok A11.4421")
-}
